929840aa28223126cb9bdeb8c93d4fee21df1637
1272086709 <1272086709@qq.com>
2026-07-02 17:24:41 +0800

    feat: 添加签名

--- src/minio/minio.controller.ts
+++ src/minio/minio.controller.ts
@@ -9,7 +9,11 @@ export class MinioController {
   @Get('presignedUrl')
   async presignedPutObject(@Query('name') name: string) {
     try {
-      const presignedUrl = await this.minioClient.presignedPutObject('public', name, 3600);
+      const presignedUrl = await this.minioClient.presignedPutObject(
+        'public', 
+        name, 
+        3600,
+      );
       console.log(presignedUrl);
       return {
         message: '预签名URL生成成功',
@@ -20,6 +24,29 @@ export class MinioController {
       };
     } catch (error) {
       console.error('Error generating presigned URL:', error);
+      throw error;
+    }
+  }
+
+  @Get('previewUrl')
+  async previewObject(@Query('name') name: string) {
+    try {
+      const previewUrl = await this.minioClient.presignedGetObject(
+        'public', 
+        name, 
+        3600,
+        { "response-content-disposition": "inline" } // 关键：内嵌预览，不下载
+      );
+      console.log(previewUrl);
+      return {
+        message: '预览URL生成成功',
+        result: true,
+        data: {
+          url: previewUrl,
+        }
+      };
+    } catch (error) {
+      console.error('Error generating preview URL:', error);
       throw error;
     }
   }
